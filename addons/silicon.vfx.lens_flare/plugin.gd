f44478e2439c4a47234ccd38fbf13aab4acec44d
yael <yael.atletl@gmail.com>
2021-01-05 21:08:56 -0600

    Updated lens flare and chromatic aberration shader

--- addons/silicon.vfx.lens_flare/plugin.gd
+++ addons/silicon.vfx.lens_flare/plugin.gd
@@ -1,12 +1,14 @@
 tool
 extends EditorPlugin
 
-func _enter_tree():
+
+func _enter_tree() -> void:
 	add_custom_type(
 		"LensFlare", "Node",
-		preload("lens-flare.gd"),
+		preload("lens_flare.gd"),
 		preload("lens_flare_icon.png")
 	)
 
-func _exit_tree():
+
+func _exit_tree() -> void:
 	remove_custom_type("LensFlare")
